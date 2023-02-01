c99e5379563e4b8d784a403071a11d3ef71881bf
Loli Kross <lizalove3535@gmail.com>
2023-02-02 00:00:19 +0500

    Добавьте файлы проекта.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Diagnostics;
+
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int n = 5000;
+            int[] a = new int[n];
+            Random rnd = new Random();
+            for (int i = 0; i < n; i++)
+                a[i] = rnd.Next(0,5000);
+            Stopwatch stpWatch = new Stopwatch();
+            stpWatch.Start();
+            int y = rnd.Next(0, 5000);
+            for (int i = 0; i < n; i++)
+            {
+                if(a[i] == y)
+                {
+                    Console.WriteLine("Число найдено");
+                    break;
+                }
+            }
+            stpWatch.Stop();
+            Console.WriteLine($"StopWatch:{stpWatch.ElapsedMilliseconds.ToString()}");
+        }
+    }
+}
