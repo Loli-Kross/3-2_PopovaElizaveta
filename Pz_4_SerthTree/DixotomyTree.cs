aae26d391a719d7fdf35c1ede3ddbd39b11d9517
Loli Kross <lizalove3535@gmail.com>
2023-03-31 10:42:45 +0500

    aaaaa

---
+++ Pz_4_SerthTree/DixotomyTree.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Pz_4_SerthTree
+{
+    class DixotomyTree
+    {
+        private DTreeNode root; 
+        public DTreeNode Root 
+        {
+            get { return root; }
+            set { root = value; }
+        }
+        public DixotomyTree() 
+        {
+            root = null;
+        }
+        public static DTreeNode Insert_DNode(DTreeNode root, int k, int a)
+        {
+            if (root == null)
+            {
+                return new DTreeNode(k, a);
+            }
+            else
+            {
+                if (k < root.Key)
+                    root.Left = Insert_DNode(root.Left, k, a);
+                else if (k > root.Key)
+                    root.Right = Insert_DNode(root.Right, k, a);
+                else
+                    Console.WriteLine($"узел с ключом {k} уже есть в дереве");
+            }
+            return root;
+        }
+
+        public static int Sum(DTreeNode treenode)
+        {
+            if (treenode == null)
+                return 0;
+            return treenode.Key + Sum(treenode.Left) + Sum(treenode.Right);
+        }
+        public static int VnutrennieNode(DTreeNode node)
+        {
+            if (node == null)
+                return 0;
+            else if (node.Left == null && node.Right == null)
+                return 0;
+            else
+                return 1 + VnutrennieNode(node.Left) + VnutrennieNode(node.Right);
+
+        }
+        public static List<int> SerchtMInNode(DTreeNode node)
+        {
+
+            List<int> result = new List<int>();
+
+            if (node == null)
+                return result;
+
+
+            if (node.Key < 0) // Проверка отрицательности
+            {
+                result.Add(node.Key);
+                Console.WriteLine($"Узел: {node.Info}, Отрицательное: {result.Count}: {node.Key} ");
+
+            }
+            else
+            {
+                Console.WriteLine($"Узел: {node.Info}, Отрицательное: {result.Count}: {node.Key} ");
+
+            }
+
+            result.AddRange(SerchtMInNode(node.Left));
+            result.AddRange(SerchtMInNode(node.Right));
+
+            return result;
+        }
+    }
+}
---
+++ Pz_4_SerthTree/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Pz_4_SerthTree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            DTreeNode root = null;
+            for (int i = 0; i < 10; i++)
+            {
+                int a = random.Next(-1000, 1000);
+                root = DixotomyTree.Insert_DNode(root, 1 + i, a);
+
+            }
+            Console.WriteLine($"Сумма инвормационных полей: {DixotomyTree.Sum(root)}");
+            Console.WriteLine();
+
+            //Задание 2
+            Console.WriteLine($"Внутренние узлы дерева: {DixotomyTree.VnutrennieNode(root)}");
+            Console.WriteLine();
+
+            //Задание 3 
+            List<int> negativeValues = DixotomyTree.SerchtMInNode(root);
+            Console.WriteLine($"Отрицательные значения информационных полей дерева: {negativeValues.Count}");
+            Console.WriteLine();
+
+            foreach (var value in negativeValues)
+            {
+                Console.Write($"{value}, ");
+            }
+            Console.WriteLine(); Console.WriteLine();
+        }
+
+    }
+}
