cb6c5af96632bad17bf253d1c1d2ccd774d4592f
Loli Kross <lizalove3535@gmail.com>
2023-04-02 00:50:50 +0500

    oh shit were go again

---
+++ Pz_6_Asinhron/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Pz_6_Asinhron
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private async void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var task = longalg(14, Status);
+            await task;
+        }
+        public async Task longalg(double a, ProgressBar p)
+        {
+            double b = 1;
+            p.Maximum = a;
+            for (int i = 0; i < a; i++)
+            {
+                b *= a;
+                p.Value += 1;
+                await Task.Delay(TimeSpan.FromSeconds(0.5));
+
+            }
+            
+        }
+
+    }
+
+}
